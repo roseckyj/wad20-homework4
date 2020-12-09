1287e271ea69589b931fe291df3ac53a389b8be4
samar23abed <samar23abed@gmail.com>
2020-12-09 17:56:00 +0200

    unlike change

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -38,12 +38,15 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+
+    PostModel.like(request.currentUser.id, request.params.postId, () => response.sendStatus(200))
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
 
+    PostModel.unlike(request.currentUser.id, request.params.postId, () => response.sendStatus(200))
 });
 
 module.exports = router;
